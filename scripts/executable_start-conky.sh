c702baa04243baa33b6e29faddfbc2680a82e50c
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-03-10 22:59:43 +0500

    merged whatstrending and timetable in left.conkyrc

--- scripts/executable_start-conky.sh
+++ scripts/executable_start-conky.sh
@@ -1,6 +1,5 @@
 pkill -f "conky -c"
-conky -c ~/.config/conky/timetable.conkyrc &
+conky -c ~/.config/conky/left.conkyrc &
 conky -c ~/.config/conky/system_info_xfce.conf &
 conky -c ~/.config/conky/weather_xfce.conf &
 conky -c ~/.config/conky/gontrihutions_xfce.conkyrc &
-conky -c ~/.config/conky/whatstrending_xfce.conkyrc &
