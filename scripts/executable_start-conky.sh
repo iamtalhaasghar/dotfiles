21c14408b1e37d9e42297b17e7804bb532876a92
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-03-10 19:03:23 +0500

    deleted conky startup files in favour of one conky bootstratp script

---
+++ scripts/executable_start-conky.sh
@@ -0,0 +1,6 @@
+pkill -f "conky -c"
+conky -c ~/.config/conky/timetable.conkyrc &
+conky -c ~/.config/conky/system_info_xfce.conf &
+conky -c ~/.config/conky/weather_xfce.conf &
+conky -c ~/.config/conky/gontrihutions_xfce.conkyrc &
+
