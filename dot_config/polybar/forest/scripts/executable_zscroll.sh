9be3a9d6fdc3f3e3d4f76832260f5e6114852c61
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-03-13 15:31:08 +0500

    backup keyboard shortcuts

--- dot_config/polybar/forest/scripts/executable_zscroll.sh
+++ dot_config/polybar/forest/scripts/executable_zscroll.sh
@@ -1,10 +1,11 @@
 # see man zscroll for documentation of the following parameters
 
-zscroll --length 35 --before-text "♪ " --delay 0.8 \
-		--match-command "playerctl status" \
+zscroll --length 35 --before-text "Online " --delay 0.8 \
+		--match-command "playerctl -s status" \
 		--match-text "Playing" "--before-text '♪ '" \
+    --match-text "" "--before-text ''" \
 		--match-text "Paused" "--before-text '[P] ' --scroll 0" \
     --match-text "Stopped" "--before-text '[S] ' --scroll 0" \
-		--update-check true "playerctl metadata title" &
+		--update-check true "playerctl -s metadata title" &
 
 wait
