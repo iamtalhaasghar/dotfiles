5d342749b29fd3f0f1fc88886cf785d091750f8c
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2023-04-11 14:14:35 +0500

    download playlist in its folder

--- scripts/ytd.bat
+++ scripts/ytd.bat
@@ -1,1 +1,1 @@
-yt-dlp -o "%%(autonumber)s-%%(title)s.%%(ext)s" -f mp4  %*
+yt-dlp -o "%%(playlist_title)s/%%(autonumber)s%%(title)s-%%(id)s.%%(ext)s" "%%(autonumber)s-%%(title)s.%%(ext)s" -f mp4  --download-archive archive.txt %* 
