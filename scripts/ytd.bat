2bccc71e6cbbf4da6176cf0df1bfba6e7a9ceaff
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2023-04-13 02:00:16 +0500

    added download location

--- scripts/ytd.bat
+++ scripts/ytd.bat
@@ -1,1 +1,2 @@
-yt-dlp -o "%%(playlist_title)s/%%(autonumber)s%%(title)s-%%(id)s.%%(ext)s" "%%(autonumber)s-%%(title)s.%%(ext)s" -f mp4  --download-archive archive.txt %* 
+yt-dlp -o "E:\%%(playlist_title)s/%%(autonumber)s-%%(title)s-%%(id)s.%%(ext)s" -f mp4  --download-archive archive.txt %* 
+yt-dlp --write-auto-sub --sub-lang en --skip-download -o "E:\%%(playlist_title)s/%%(autonumber)s-%%(title)s-%%(id)s.%%(ext)s" %*
